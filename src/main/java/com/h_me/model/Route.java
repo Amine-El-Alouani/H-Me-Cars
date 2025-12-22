01b1c8713d94bdaec15f0e6ad3be51cbb02f1c43
AchrafAchoo <achraf.alla38@gmail.com>
2025-12-22 21:07:28 +0100

    added airline, routes and user models

---
+++ src/main/java/com/h_me/model/Route.java
@@ -0,0 +1,17 @@
+package com.h_me.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Route {
+    private String flightNum;
+    private int airlineID;
+    private int sourceID;
+    private int destID;
+    private String Equipement;
+}
